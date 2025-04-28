4d6571716f9154776aaabafa9d0fe38eaf821ea7
toosynxed <bconomy.wiki@gmail.com>
2025-04-28 16:15:53 +1000

    Update v.2
    Major changes to files.
    
    Command line code:
    The previous "commandline.py" file has now been replaced and renamed as "commandline(out-of-date).py". The current file named "commandline.py" has been implemented as it is much more efficient, simple, less cluttered, and generally more well structured than previous versions.
    New "commandline.py" utilises set() instead of needing copious amounts of coding to separate and search through the "chosen_word" variable every time there is a new input. This will significantly reduce not only the amount of code looping that occurs, but also the effect on the program resources that are required to perform the task.
    
    .txt files:
    Created .txt files to store words/alphabet letters that can be changed/altered easily without affecting code.
    
    GUI Renaming:
    Renamed loose GUI files to better represent content.

---
+++ commandline.py
@@ -0,0 +1,57 @@
+import os
+import random
+os.system('clear') #Clear terminal
+alphabet = open(r"alphabet.txt","r") #call file
+guessed_letters = [] #Initialises list
+letters_remaining = [] #Initialises list
+word_choices = ['apple', 'banana', 'orange', 'pear']
+chosen_word = random.choice(word_choices) #Picks a random word from the list
+word = set(chosen_word) #Finds all distinct characters
+print(chosen_word)
+print(word)
+lives = len(chosen_word) #Sets the life value  ***** LATER CHANGE THIS TO SET VALUE AFTER CREATING IMAGE OF HOUSE *****
+
+for x in alphabet: #Appending letters from alphabet file to the list
+    letters_remaining.append(x.replace('\n', ''))
+
+#print(f'Word Status: {' '.join(['_' if letter not in guessed_letters else letter for letter in chosen_word])}')
+
+while lives != 0 and len(word) != 0: #While game is still valid, and not won or lost
+    print(f'Letters Remaining: {' '.join(letters_remaining)}') #Letters left to guess
+    print(f'Letters Guessed: {' '.join(guessed_letters)}') #Letters already guessed
+    print(f'Word Status: {' '.join(['_' if letter not in guessed_letters else letter for letter in chosen_word])}') #Prints out underscored word
+    
+    letter = input('Pick a letter: ').lower() #Input line
+    os.system('clear')
+    if letter not in guessed_letters and len(letter) == 1 and letter.isalpha() == True: #Makes sure that it hasn't been guessed already, is only 1 character long, is a valid letter
+        letters_remaining.remove(letter) #Removes letter from alphabet
+        guessed_letters.append(letter) #Adds letter to the list of already guessed letters
+
+       # print(f'Wordd Status: {' '.join(['_' if letter not in guessed_letters else letter for letter in chosen_word])}')
+
+        if letter in word: #Checks if the letter is in the set
+            #print(f'{' '.join(['_' if letter not in guessed_letters else letter for letter in chosen_word])}')
+
+            word.remove(letter) #Removes the correctly guessed letter from the set
+            #letters_remaining.remove(letter)
+            print('Good Job! You have guessed a correct letter!')
+            print(f'You have {lives} lives remaining!')
+            #print(f'Letters Remaining: {' '.join(letters_remaining)}') #Letters left to guess
+            #print(f'Letters Guessed: {' '.join(guessed_letters)}') #Letters already guessed
+
+        elif letter not in word: #If not in set
+            lives = lives - 1 #Lose a life, and image changes ****** IMAGE CHANGES HERE PLEASE!!! ******
+            print(f'Uh, Oh!\nIt seems that this letter is not in the word!\nYou have {lives} lives remaining!') #Feedback off incorrect guess
+
+    elif letter.isalpha() == False or len(letter) != 1: #If input isn't a letter or only 1 character long
+        print(f'Uh, Oh!\nInvalid Input!\nPick again!')
+
+    elif letter in guessed_letters: #If input has already been guessed before
+        print(f'Uh, Oh!\nIt seems that you have already guessed this letter!\nPick again!')
+
+if lives <= 0: #Lose condition
+    print(f'Oh, No!\nIt seems that you were not able to save the house in time!\nRestart to play again!')
+
+elif len(word) == 0: #Win condition
+    os.system('clear')
+    print(f'Good Job!\nYou managed to save the house from being destroyed!\nThe word was: {chosen_word}\nRestart to play again!')
--- gui.py
+++ gui.py
@@ -1,10 +1,68 @@
-import tkinter
-from tkinter.constants import *
-tk = tkinter.Tk()
-frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
-frame.pack(fill=BOTH,expand=1)
-label = tkinter.Label(frame, text="Hello, World")
-label.pack(fill=X, expand=1)
-button = tkinter.Button(frame,text="Exit",command=tk.destroy)
-button.pack(side=BOTTOM)
-tk.mainloop()
+from tkinter import *
+import random
+from tkinter import messagebox
+#root = tk.Tk()
+#root = Tk()
+#root.title("Guess The Word!")
+#root.geometry("500x500")
+
+#manlabel = Label(root, font=("CourierK", 16))
+#manlabel.grid(row=0, column=0)
+#manlabel.config(text="Welcome to Build A House!")
+#word_choices = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+#word = random.choice(word_choices)
+#word = 'cherry' #just here to ensure that during testing, all variables remain the same
+#word_in_list_form = list(word)
+
+#length_of_chosen_word = len(word)
+#display_letters = []
+#display_letters = word_in_list_form
+#for i in range(0,length_of_chosen_word):
+#	display_letters[i] = '_'	
+#already_guessed = []
+#correct_score = 0
+#chn = 0
+#picked = 0
+#strike = 0
+#underscore_display_letters = []
+#underscore_display_letters = ' '.join(display_letters)
+class House:
+    #picked = 0
+    def __init__(self,master):
+        self.count=0
+        self.structure(master)
+        self.rr=master
+        
+        #Input function#
+    def structure(self, master, play, word, display_letters, already_guessed, strike):
+        self.yes = play
+        self.chosen = word
+        self.display = display_letters
+        self.guessed = already_guessed
+        self.strike_out = strike
+
+    def add(self,master,play,word):
+        return play + word
+dog = House(1)
+dog = House.structure(1,2,3,4,5,6,7,8,9)
+print(dog.add())
+
+
+
+    
+    
+
+
+
+		
+#word_label = tk.Label(root, text=underscore_display_letters, font=("Arial", 24))
+#word_label.grid(row=102, column=0)  
+
+
+#root.geometry("580x480")
+
+
+#app = House(root)
+      
+#root.mainloop()
+
